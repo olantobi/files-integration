31e2d906247b4a5b08f9d38a1c63dcb2a8210b05
Tobi Olanrewaju <olantobi@gmail.com>
2019-09-10 20:58:54 +0100

    Initial code base from spring initializr

---
+++ src/main/java/com/liferon/filesintegration/FilesIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.liferon.filesintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesIntegrationApplication.class, args);
+	}
+
+}
